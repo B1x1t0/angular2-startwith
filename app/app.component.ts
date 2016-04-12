2bd171c3f93c942c1c0f8682a4e35551aed2be21
Inma <inma@traffgen.com>
2016-04-12 16:28:18 +0200

    Cat name input

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,26 @@
 import {Component} from 'angular2/core';
 
+export class Cat {
+  id: number;
+  name: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `<h1>Welcome to {{title}}</h1>
+            <h2>{{cat.name}} details!</h2>
+            <div><label>id: </label>{{cat.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="cat.name" placeholder="name">
+            </div>`
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Cats App';
+
+  cat: Cat = {
+    id: 1,
+    name: 'Lolo'
+  }
+}
